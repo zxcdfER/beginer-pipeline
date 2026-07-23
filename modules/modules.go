369a4b91cb7f4f09488295126f1e386d22e50827
zxcdfER <z49955256@gmail.com>
2026-07-24 00:35:51 +0300

    ohh im stupid

--- modules/modules.go
+++ modules/modules.go
@@ -2,7 +2,6 @@ package modules
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -12,70 +11,11 @@ type Filestruct struct {
 	Filename string
 	Filter   string
 }
-type Read interface {
+type Fl interface {
+	Filt(<-chan string) <-chan string
 	Readfile() (<-chan string, error)
 }
-type Filt interface {
-	Filt(<-chan string) <-chan string
-}
-type Fl interface {
-	Read
-	Filt
-}
-type Jsonstruct struct {
-	Filename string
-	Filter   string
-}
 
-func ras(filenames, filters string) Fl {
-	zxc := strings.SplitN(filenames, ".", 2)
-	switch zxc[1] {
-	case "json":
-		return Jsonstruct{Filename: filenames, Filter: filters}
-	case "txt", "log":
-		return Filestruct{Filename: filenames, Filter: filters}
-	default:
-		return nil
-	}
-}
-func (js Jsonstruct) Readfile() (<-chan string, error) {
-	file, err := os.Open(js.Filename)
-	if err != nil {
-		return nil, fmt.Errorf("The error:%w", err)
-	}
-	out := make(chan string)
-	reader := bufio.NewReader(file)
-	go func() {
-		defer close(out)
-		defer file.Close()
-		for {
-			str, err := reader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			var data map[string]interface{}
-			err = json.Unmarshal([]byte(str), &data)
-			var path []string
-			for i, el := range data {
-				path = append(path, fmt.Sprintf("%s:%v", i, el))
-			}
-			out <- strings.Join(path, " ")
-		}
-	}()
-	return out, nil
-}
-func (js Jsonstruct) Filt(out <-chan string) <-chan string {
-	exits := make(chan string)
-	go func() {
-		defer close(exits)
-		for el := range out {
-			if strings.Contains(strings.ToLower(el), strings.ToLower(js.Filter)) {
-				exits <- el
-			}
-		}
-	}()
-	return exits
-}
 func (Zxcm Filestruct) Filt(out <-chan string) <-chan string {
 	exits := make(chan string)
 	go func() {
